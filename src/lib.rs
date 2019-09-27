dfb5ef08fe5dd01aa7a5cb2acf573629ebdf3239
sagie gur ari <sagiegurari@gmail.com>
2019-09-27 12:13:46 +0300

    update lint rules

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,6 @@
     legacy_constructor_visibility,
     legacy_directory_ownership,
     macro_expanded_macro_exports_accessed_by_absolute_paths,
-    meta_variable_misuse,
     missing_copy_implementations,
     missing_docs,
     missing_fragment_specifier,
@@ -198,11 +197,6 @@
 #[cfg(test)]
 #[path = "./lib_test.rs"]
 mod lib_test;
-
-extern crate rand;
-
-#[cfg(not(windows))]
-extern crate users;
 
 #[macro_use]
 mod macros;
